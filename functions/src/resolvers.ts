8b9499c65a5f14f10e7a05c64ae7325e6072ff64
ioetbc <ioetbc@gmail.com>
2022-10-19 16:59:54 +0100

    return the correct proficiency levels tooooo

--- functions/src/resolvers.ts
+++ functions/src/resolvers.ts
@@ -13,20 +13,32 @@ export const resolvers = {
       });
     },
     findUsers: async (): Promise<User[]> => {
-      const filters = ["react", "node"];
+      const where = {
+        OR: [
+          {
+            technology: "react",
+            proficiency: {
+              gte: 10,
+            },
+          },
+          {
+            technology: "node",
+            proficiency: {
+              gte: 10,
+            },
+          },
+        ],
+      };
+
       const gmm = await prisma.technology.groupBy({
         by: ["userId"],
 
         _count: true,
-        where: {
-          technology: {
-            in: filters,
-          },
-        },
+        where,
       });
 
       const filtered = gmm
-        .filter((item) => item._count === filters.length)
+        .filter((item) => item._count === where.OR.length)
         .map((item) => item.userId) as number[];
 
       const users = prisma.user.findMany({
